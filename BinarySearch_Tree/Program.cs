ee7ad739d16261d26d8a9a629e73046e039bb992
pampapathi73 <pampapathik32@gmail.com>
2021-06-14 21:35:24 +0530

    [pampapathi] Added: Welcome binary search tree

---
+++ BinarySearch_Tree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearch_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+        }
+
+    }
+}
